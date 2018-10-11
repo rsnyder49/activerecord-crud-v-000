64463e73ac8817d31cea4588c72b5978b7fd8c4d
Robert Snyder <r.snyder49@gmail.com>
2018-10-11 21:34:58 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -78,7 +78,7 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   movie = Movie.create(title: "Wat?")
   movie.title = "Wat, huh?"
-  movie.update
+  movie.save
 end
 
 def can_update_multiple_items_at_once
